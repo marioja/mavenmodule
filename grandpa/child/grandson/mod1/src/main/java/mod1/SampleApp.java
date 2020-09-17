cd3e020ed9b81931d6de92221ae118da63b2a1ae
Mario Jauvin <mfj@earthling.net>
2020-09-17 09:51:57 -0400

    Created sample multi-module maven project

---
+++ grandpa/child/grandson/mod1/src/main/java/mod1/SampleApp.java
@@ -0,0 +1,14 @@
+package mod1;
+
+public class SampleApp {
+
+	public static void main(String[] args) {
+		SampleApp sa=new SampleApp();
+		sa.greetings();
+
+	}
+	private void greetings() {
+		System.out.println("This is the sample app running.");
+	}
+
+}
